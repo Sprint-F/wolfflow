4c6b276416a707b659f92aff14eb466e09b6c975
Albert Stepantsev <albert.stepantsev@sprintf.ru>
2025-02-22 20:08:40 +0300

    + SensitiveParameter using in contexts

--- src/Context/ContextAbstract.php
+++ src/Context/ContextAbstract.php
@@ -9,6 +9,25 @@ abstract class ContextAbstract implements ContextInterface
 {
     public function jsonSerialize(): array
     {
-        return get_object_vars($this);
+        $vars = get_object_vars($this);
+
+        foreach ($vars as $key => &$value) {
+            $property = new \ReflectionProperty($this, $key);
+            $isSensitive = !empty($property->getAttributes(\SensitiveParameter::class));
+            if (!$isSensitive) {
+                $constructorParams = (new \ReflectionClass($this))->getConstructor()->getParameters();
+                foreach ($constructorParams as $param) {
+                    if ($param->isPromoted() && $key == $param->getName()) {
+                        $isSensitive = !empty($param->getAttributes(\SensitiveParameter::class));
+                        break;
+                    }
+                }
+            }
+            if ($isSensitive) {
+                $value = '***';
+            }
+        }
+
+        return $vars;
     }
 }
